22b0c5721a654b05d50eb5eba65426f112082b6d
jdpulido <127062654+jdpulido1906@users.noreply.github.com>
2023-09-19 21:13:50 -0500

    Update Footer.js

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -2,9 +2,6 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <div className="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-      </div>
       <strong>
         Copyright © 2023 <a href="https://adminlte.io">miDulceOnline</a>.
       </strong>{" "}
